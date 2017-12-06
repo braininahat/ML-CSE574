326eb2a1e7e81621a41ee4c0fa06df91956c6a1e
Varun Shijo <varunshi@buffalo.edu>
2017-12-05 23:15:30 -0500

    Done. TODO: Minor cleaning and commenting.

--- 4/code/main.py
+++ 4/code/main.py
@@ -1,77 +1,155 @@
-from keras.preprocessing.image import ImageDataGenerator
-from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D
-from keras.layers import Activation, Dropout, Flatten, Dense
+import pandas as pd
+from glob import glob
+from os import makedirs, mkdir, fsync
+from shutil import copy2
+from keras.utils import to_categorical
+from keras.models import Sequential, load_model
+from keras.layers import Conv2D, MaxPooling2D, Activation, Dropout, Flatten, Dense
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
 from keras import backend as K
 
+############## DIRECTORIES #############
 
-# dimensions of our images.
-img_width, img_height = 178, 218
+train_image_dir = '../data/training'
+validation_image_dir = '../data/validation'
+test_image_dir = '../data/test'
 
-train_data_dir = '../data/training'
-validation_data_dir = '../data/validation'
-nb_train_samples = 2000
-nb_validation_samples = 800
-epochs = 50
+############## ARGS ####################
+
+filter_size = (3, 3)
 batch_size = 16
+height, width = 218, 178
 
 if K.image_data_format() == 'channels_first':
-    input_shape = (3, img_width, img_height)
+    input_shape = (3, width, height)
 else:
-    input_shape = (img_width, img_height, 3)
+    input_shape = (width, height, 3)
 
-model = Sequential()
-model.add(Conv2D(32, (3, 3), input_shape=input_shape))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+################ LABELS #################
+try:
+    load_model('../data/celeba.h5')
+except:
+    original = open('../data/list_attr_celeba.txt', 'r')
+    cleaned = open('../data/cleaned.txt', 'w')
+    text = original.readlines()
+    converted = [line.replace('-1', '0') for line in text[1:]]
+    header_old = converted[0]
+    header_new = 'Filename ' + header_old
+    converted[0] = header_new
+    stripped = [line.replace('  ', ' ') for line in converted]
+    [cleaned.write(line) for line in stripped]
+    original.flush()
+    cleaned.flush()
+    fsync(original.fileno())
+    fsync(cleaned.fileno())
+    original.close()
+    cleaned.close()
 
-model.add(Conv2D(32, (3, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+    makedirs('../data/test/0', mode=0o777)
+    mkdir('../data/test/1', mode=0o777)
+    makedirs('../data/training/0', mode=0o777)
+    mkdir('../data/training/1', mode=0o777)
+    makedirs('../data/validation/0', mode=0o777)
+    mkdir('../data/validation/1', mode=0o777)
 
-model.add(Conv2D(64, (3, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+    df = pd.read_table('../data/cleaned.txt', delimiter=' ')
+    print(df.head())
 
-model.add(Flatten())
-model.add(Dense(64))
-model.add(Activation('relu'))
-model.add(Dropout(0.5))
-model.add(Dense(1))
-model.add(Activation('sigmoid'))
+    labels = df['Eyeglasses'].astype(int)
 
-model.compile(loss='binary_crossentropy',
-              optimizer='rmsprop',
-              metrics=['accuracy'])
+    label_count = len(labels)
 
-# this is the augmentation configuration we will use for training
-train_datagen = ImageDataGenerator(
-    rescale=1. / 255,
-    shear_range=0.2,
-    zoom_range=0.2,
-    horizontal_flip=True)
+    unit = int(label_count / 10)
+    train_count = unit * 8
+    validation_count = unit * 9
 
-# this is the augmentation configuration we will use for testing:
-# only rescaling
-test_datagen = ImageDataGenerator(rescale=1. / 255)
+    train_labels = labels[0:train_count]
+    validation_labels = labels[train_count + 1:validation_count]
+    test_labels = labels[validation_count + 1:]
 
-train_generator = train_datagen.flow_from_directory(
-    train_data_dir,
-    target_size=(img_width, img_height),
-    batch_size=batch_size,
-    class_mode='binary')
+    img_list = sorted(glob('../data/img_align_celeba/*'))
 
-validation_generator = test_datagen.flow_from_directory(
-    validation_data_dir,
-    target_size=(img_width, img_height),
-    batch_size=batch_size,
-    class_mode='binary')
+    img_count = len(img_list)
 
-model.fit_generator(
-    train_generator,
-    steps_per_epoch=nb_train_samples // batch_size,
-    epochs=epochs,
-    validation_data=validation_generator,
-    validation_steps=nb_validation_samples // batch_size)
+    unit = int(img_count / 10)
 
-model.save_weights('first_try.h5')
+    train_bound = 8 * unit
+    validation_bound = 9 * unit
+
+    train_img_names = img_list[0:train_bound]
+    validation_img_names = img_list[train_bound + 1:validation_bound]
+    test_img_names = img_list[validation_bound + 1:]
+
+    for counter in range(len(train_img_names)):
+        copy2(train_img_names[counter], '../data/training/' +
+              str(train_labels[counter]))
+
+    for counter in range(len(validation_img_names)):
+        copy2(validation_img_names[counter], '../data/validation/' +
+              str(validation_labels[counter + train_count + 1]))
+
+    for counter in range(len(test_img_names)):
+        copy2(test_img_names[counter], '../data/test/' +
+              str(test_labels[counter + validation_count + 1]))
+
+
+    # sorted till here
+
+    ################ NETWORK #################
+
+    model = Sequential()
+    model.add(Conv2D(32, filter_size, input_shape=input_shape))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(32, filter_size))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(64, filter_size))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
+    model.add(Dense(64))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(1))
+    model.add(Activation('sigmoid'))
+
+    model.compile(loss='binary_crossentropy',
+                  optimizer='rmsprop',
+                  metrics=['accuracy'])
+
+    ################ TRAINING ##############
+
+    train_datagen = ImageDataGenerator(
+        rescale=1. / 255,
+        # shear_range=0.2,
+        # zoom_range=0.2,
+        # horizontal_flip=True)
+    )
+
+    test_datagen = ImageDataGenerator(rescale=1./255)
+
+    train_generator = train_datagen.flow_from_directory(
+        train_image_dir,
+        target_size=(width, height),
+        batch_size=batch_size,
+        class_mode='binary')
+
+    # this is a similar generator, for validation data
+    validation_generator = test_datagen.flow_from_directory(
+        validation_image_dir,
+        target_size=(width, height),
+        batch_size=batch_size,
+        class_mode='binary')
+
+    model.fit_generator(
+        train_generator,
+        steps_per_epoch=2000 // batch_size,
+        epochs=50,
+        validation_data=validation_generator,
+        validation_steps=800 // batch_size)
+
+    model.save('celeba.h5')
